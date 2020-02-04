501c1d07742bfaceb4a763013a196f983a45dec4
Craig Barber <craigb@mojotech.com>
2020-02-03 19:56:39 -0500

    fix: move all docs to a sub path and redirect

---
+++ src/routes/docs/index.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  export async function preload() {
+    const sections = await this.fetch(`docs.json`).then(r => r.json());
+    return { sections };
+  }
+</script>
+
+<script>
+  import Docs from '@sveltejs/site-kit/components/Docs.svelte'
+  export let sections;
+</script>
+
+<svelte:head>
+  <title>Mojodocs</title>
+
+  <meta name="twitter:title" content="Mojodocs">
+  <meta name="twitter:description" content="Usefull docs for mojos">
+  <meta name="description" content="Usefull docs for mojos">
+</svelte:head>
+
+<Docs {sections} project="sapper"/>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,5 @@
 <script context="module">
-  export async function preload() {
-    const sections = await this.fetch(`docs.json`).then(r => r.json());
-    return { sections };
+  export async function preload(page, session) {
+    return this.redirect(302, 'docs');
   }
 </script>
-
-<script>
-  import Docs from '@sveltejs/site-kit/components/Docs.svelte'
-  export let sections;
-</script>
-
-<svelte:head>
-  <title>Mojodocs</title>
-
-  <meta name="twitter:title" content="Mojodocs">
-  <meta name="twitter:description" content="Usefull docs for mojos">
-  <meta name="description" content="Usefull docs for mojos">
-</svelte:head>
-
-<Docs {sections} project="sapper"/>
